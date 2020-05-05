550667742aa2be967b42c78b0193e0f6e5721fff
jngmnghn <jeongmyeonghyeon@gmail.com>
2020-05-05 22:13:27 +0900

    add post dummy models & custom admin

--- app/config/settings/common.py
+++ app/config/settings/common.py
@@ -15,7 +15,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 
 MEDIA_URL = '/media/'
-MEDIA_DIR = os.path.join(ROOT_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
--- app/instagram/admin.py
+++ app/instagram/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Post, Comment, Tag
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['photo_tag', 'caption']
+    list_display_links = ['caption']
+
+    def photo_tag(self, post):
+        # return f'<img src={post.photo.url}>' # 이렇게 처리하면, django 보안에 의해 < 가 lgt; 로 변환됨. -> html 태그로 인식안함.
+        return mark_safe(f'<img src={post.photo.url} width="80px">') # 보안상 문제가 생길 수 있긴 함. 안전하다고 판단되는 경우에만 사용해야 함.
 
 
 @admin.register(Comment)
